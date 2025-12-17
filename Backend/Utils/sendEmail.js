68c0164a2f617ea7772f41a4f3569718bc949e3d
Kunalverma001 <kunalverma6336@gmail.com>
2025-12-18 01:03:15 +0530

    changed domain name in sendemail

--- Backend/Utils/sendEmail.js
+++ Backend/Utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to, subject, text) {
     try {
         await resend.emails.send({
-            from: "Stock Trading <onboarding@resend.dev>",
+            from: "Stock Trading <no-reply@zerotrade-kunalverma.site>",
             to,
             subject,
             html: `<p>${text}</p>`,
